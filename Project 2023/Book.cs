4bb95bb19e44f0c03c2a1d0e2009c8207da9696b
Capsaicin1 <capsaicin1@outlook.com>
2023-06-06 10:35:46 +1200

    Created my BoodModel
    Creating a new class to represent a book in my code.

---
+++ Project 2023/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_2023
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string author { get; set; }
+        public string genre { get; set; }
+        public int hasRead { get; set; }
+
+    }
+}
