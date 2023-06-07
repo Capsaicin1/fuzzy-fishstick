b1fd710966fa771d1f834a4b18dc2509ff41b297
Capsaicin1 <capsaicin1@outlook.com>
2023-06-07 12:06:37 +1200

    Create SqlDataAccess class
    Created the class and added the code for two basic sql queries to test that the connection works.

---
+++ Project 2023/SqliteDataAccess.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using Dapper;
+using System.Data;
+using System.Configuration;
+
+namespace Project_2023
+{
+    // A seperate class to access my database. I will write all of my SQL queries in this class.
+    public class SqliteDataAccess
+    {
+        public static List<Book> LoadPeople()
+        {
+            using (IDbConnection cnn  = new SQLiteConnection(LoadConnectionString()))
+            {
+                var output = cnn.Query<Book>("select * from Books", new DynamicParameters());
+                return output.ToList();
+            }
+        }
+
+        public static void saveBook(Book book)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(LoadConnectionString()))
+            {
+                cnn.Execute("insert into Books (Title, Author, Genre, hasRead) values (@Title, @Author, @Genre, @hasRead)", book);
+            }
+        }
+
+        private static string LoadConnectionString(string id = "Default")
+        {
+            return ConfigurationManager.ConnectionStrings[id].ConnectionString;
+        }
+    }
+}
